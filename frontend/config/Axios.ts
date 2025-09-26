a7eba7661b79f89d1815a3ff23f2a917afaa2668
Harsh <gamerpandeyharsh@gmail.com>
2025-09-26 12:45:41 +0530

    Added Axios Instance properly

---
+++ frontend/config/Axios.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+AxiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default AxiosInstance;
